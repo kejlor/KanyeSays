5508ac7d069aa17ad29eca2dc0dde27c8a6f0a8c
kejlor <woj450@gmail.com>
2021-12-29 18:32:54 +0100

    Initial Commit

---
+++ KanyeSays/KanyeSaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanyeSaysApp.swift
+//  KanyeSays
+//
+//  Created by Bartosz Wojtkowiak on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct KanyeSaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
